50e57748555e90c1e9741ec8e165abe0a7f15271
Ameer Adhami <ameer.adhami@optimumpartners.co>
2021-01-10 16:33:00 +0200

    add models.address tests

--- test/models/test_address_models.py
+++ test/models/test_address_models.py
@@ -1,15 +1,13 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'src'))
-
 from unittest.mock import patch
 from models.address import Address
 
 @patch('models.engine')
 def test_as_json(mockEngine, mock_address_request_data):
     request_data = mock_address_request_data.copy()
-    request_data['created_at'] = None
-    request_data['last_updated'] = None
 
     address = Address(**request_data)
+
+    request_data['created_at'] = None
+    request_data['last_updated'] = None
 
     assert address.as_json() == request_data
