eb8ca580184283159e2aeea3ab4ad1b31b48eeb3
Ameer Adhami <ameer.adhami@optimumpartners.co>
2021-01-12 14:25:31 +0200

    add main test and cov

--- bin/test.sh
+++ bin/test.sh
@@ -2,4 +2,4 @@
 
 cd test
 
-pytest -s --color=yes -v -rA --cov-report=term-missing --cov-fail-under=100 --cov=../src/controllers --cov=../src/models
+pytest -s --color=yes -v -rA --cov-report=term-missing --cov-fail-under=100 --cov=../src/controllers --cov=../src/models --cov=main
