6f0c14d30b87fd12cd429a526147bf3f045c09db
Ameer Adhami <ameer.adhami@optimumpartners.co>
2021-01-12 11:14:39 +0200

    more travis

--- bin/build_test.sh
+++ bin/build_test.sh
@@ -2,4 +2,4 @@
 
 docker build -t fastapi-app-test --build-arg EXTRA_REQUIREMENTS=test-requirements.txt .
 
-docker exec fastapi-app-test -t ./bin/test.sh
+docker run fastapi-app-test -t ./bin/test.sh
